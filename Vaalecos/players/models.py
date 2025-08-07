58c1d990507ba1c448e4fa49689540c46ce9a3da
Tuwane <152686997+Tuwane@users.noreply.github.com>
2025-08-07 22:53:32 +0200

    udpdting files edding new features

--- Vaalecos/players/models.py
+++ Vaalecos/players/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
