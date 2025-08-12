9811801e69223acf3a1771d1e70caa2373c9d754
Tuwane <152686997+Tuwane@users.noreply.github.com>
2025-08-12 22:24:00 +0200

    edding additional files updatring

--- Vaalecos/players/admin.py
+++ Vaalecos/players/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Player
+
+
 # Register your models here.
+
+
+
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ("firstname", "lastname", "joined_date",)
 admin.site.register(Player, PlayerAdmin)
 
+admin.site.site_header = "Vall ecos Football Club Admin"
+admin.site.site_title = "Vall ecos Football Club Portal"
+admin.site.index_title = "Wellcome to Vall Ecoes FC Management Panel"
 
